78d83a16a36d70bee0cd619ac03e27b4b10d0211
peroin <milliarsatu4@gmail.com>
2026-05-26 14:43:21 +0700

    update: tambahkan tim editor dan disclaimer artikel

---
+++ src/data/team.js
@@ -0,0 +1,26 @@
+// src/data/team.js
+export const teamMembers = [
+  { 
+    name: "Iqbal Maulana", 
+    role: "Pakar Crypto & Blockchain", 
+    fb: "https://www.facebook.com/share/18kcTm8hKy/", 
+    ig: "https://www.instagram.com/iqbalmaulanas_?igsh=dGQyZTducGI2NXMw" 
+  },
+  { 
+    name: "Siti Rohimah", 
+    role: "Riset Data Crypto & RWA", 
+    fb: "https://www.facebook.com/share/1JVtw65hVv/", 
+    ig: "https://www.instagram.com/sitirohimah08_?igsh=MWpocTJkaTl1MHF5dQ==" 
+  },
+  { 
+    name: "Galuh Purnama", 
+    role: "Analis Pasar Crypto", 
+    ig: "https://www.instagram.com/luhhhhh666?igsh=MXh5cjBuanBidHhvdw==" 
+  },
+  { 
+    name: "Claudia arista", 
+    role: "Analis Profesional", 
+    bio: "Sejak SMP sudah menyukai menulis artikel dan sekarang bekerja sebagai analis profesional.",
+    fb: "https://www.facebook.com/share/1FnU3uM8CL/"
+  }
+];
