f57b6691d177f21a830c9c52f44bfc9b97ac6bea
peroin <milliarsatu4@gmail.com>
2026-05-27 15:57:19 +0700

    update: improve readmore component and article body styling

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -1,9 +1,17 @@
 // src/lib/sanity.ts
 import { createClient } from '@sanity/client';
+import { createImageUrlBuilder } from '@sanity/image-url'; // Gunakan named export ini
 
 export const client = createClient({
   projectId: '1dgx0sfa',
   dataset: 'production',
   apiVersion: '2025-05-22',
-  useCdn: true, // Gunakan true untuk kecepatan akses global
+  useCdn: true,
 });
+
+// Gunakan createImageUrlBuilder yang baru
+const builder = createImageUrlBuilder(client);
+
+export function urlFor(source: any) {
+  return builder.image(source);
+}
